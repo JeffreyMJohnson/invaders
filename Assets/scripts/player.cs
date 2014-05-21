38f6209b21fe1a0e9dbe68f80d4f53a41ad04ce7
Jeffrey M. Johnson <jefjohms@gmail.com>
2014-05-20 22:41:18 -0700

    gameboard created and player added.

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour
+{
+		public float speed;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				float translationX = Input.GetAxis ("Horizontal") * speed;
+				translationX *= Time.deltaTime;
+				Debug.Log ("screen width: " + Screen.width);
+				transform.Translate (new Vector3 (translationX, 0, 0));
+		}
+}
