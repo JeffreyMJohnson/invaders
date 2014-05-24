5bf1970a929d0b72b3a3048eea297b5fb0e8cfd2
Jeffrey M. Johnson <jefjohms@gmail.com>
2014-05-23 21:55:14 -0700

    resolution independant player movement

---
+++ Assets/scripts/camera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour
+{
+		public int livesLabelPaddingTop;
+		public int livesLabelPaddingLeft;
+		public Font livesLabelFont;
+
+		public Transform rightWall;
+		public Transform leftWall;
+
+		void OnGUI ()
+		{
+				GUI.contentColor = Color.green;
+				GUIStyle style = new GUIStyle ();
+				style.padding.top = livesLabelPaddingTop;
+				style.padding.left = livesLabelPaddingLeft;
+				style.font = livesLabelFont;
+				style.normal.textColor = Color.green;
+				GUI.Label (new Rect (0, 0, 50, 50), "Lives:", style);
+		}
+
+		void Start ()
+		{
+				BoxCollider2D collider = GetComponent<BoxCollider2D> ();
+				Vector2 size = collider.size;
+				//Debug.Log ("collider size: " + size);
+				
+				//Debug.Log ("cam width: " + camera.pixelWidth);
+		}
+}
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -1,22 +1,47 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class player : MonoBehaviour
 {
 		public float speed;
+		public Camera camera;
+
+		private float playerSize;
+		private Vector3 screenWidthWorldPos;
 
 		// Use this for initialization
 		void Start ()
 		{
+				//actually half the size
+				playerSize = renderer.bounds.extents.x;
+				Debug.Log ("playerSize: " + playerSize);
+				
 	
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-				float translationX = Input.GetAxis ("Horizontal") * speed;
-				translationX *= Time.deltaTime;
-				Debug.Log ("screen width: " + Screen.width);
-				transform.Translate (new Vector3 (translationX, 0, 0));
+				float translation = Input.GetAxis ("Horizontal");
+
+				transform.Translate (new Vector3 (translation * speed, 0.0f, 0.0f));
+				//Debug.Log ("transPos: " + transform.position);
+				Vector3 maxX = transform.position + new Vector3 (playerSize, 0, 0);
+				Vector3 minX = transform.position - new Vector3 (playerSize, 0, 0);
+				//Debug.Log ("adjPos:" + adjPos);
+				Vector3 viewPosMax = Camera.main.WorldToViewportPoint (maxX);
+				Vector3 viewPosMin = Camera.main.WorldToViewportPoint (minX);
+				//Debug.Log ("viewPos: " + viewPos);
+				viewPosMax.x = Mathf.Clamp01 (viewPosMax.x);
+				viewPosMin.x = Mathf.Clamp01 (viewPosMin.x);
+				//Debug.Log ("new pos: " + Camera.main.ViewportToWorldPoint (viewPos));
+				if (viewPosMax.x == 1.0f) {
+						float adjustMax = Camera.main.ViewportToWorldPoint (viewPosMax).x - playerSize;
+						transform.position = new Vector3 (adjustMax, transform.position.y, transform.position.z);
+				} else if (viewPosMin.x == 0.0f) {
+						float adjustMin = Camera.main.ViewportToWorldPoint (viewPosMin).x + playerSize;
+				
+						transform.position = new Vector3 (adjustMin, transform.position.y, transform.position.z);
+				}
 		}
 }
