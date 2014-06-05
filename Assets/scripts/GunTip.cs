59c8476496395b75539c17e805f82c40714a0553
Jeffrey M. Johnson <jefjohms@gmail.com>
2014-06-05 10:25:51 -0700

    reverted delet

---
+++ Assets/scripts/GunTip.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunTip : MonoBehaviour
+{
+		public float fireRate = .25f;
+		public GameObject shotPrefab;
+
+		private float nextFire = 0.0f;
+		
+		// Use this for initialization
+		void Start ()
+		{
+				Debug.Log ("gunTip started");
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				fire ();
+		}
+
+		void fire ()
+		{
+				//Debug.Log (Input.GetButtonDown ("Fire1"));
+		
+				if (Input.GetButtonUp ("Fire1") && Time.time > nextFire) {
+						nextFire = Time.time + fireRate;
+						Instantiate (shotPrefab, transform.position, Quaternion.identity);
+				}
+		
+		}
+}
---
+++ Assets/scripts/camera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour
+{
+		public int livesLabelPaddingTop;
+		public int livesLabelPaddingLeft;
+		public Font livesLabelFont;
+
+		void OnGUI ()
+		{
+				GUI.contentColor = Color.green;
+				GUIStyle style = new GUIStyle ();
+				style.padding.top = livesLabelPaddingTop;
+				style.padding.left = livesLabelPaddingLeft;
+				style.font = livesLabelFont;
+				style.normal.textColor = Color.green;
+				GUI.Label (new Rect (0, 0, 50, 50), "Lives:", style);
+		}
+
+		void Start ()
+		{
+			
+		}
+}
